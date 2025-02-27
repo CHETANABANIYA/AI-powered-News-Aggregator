bd649070c4ebef8e0b4eeebbe4b2f2d4d80c9091
Chetana Baniya <chetanabaniya5@gmail.com>
2025-02-27 20:33:45 +0530

    Updated fetchNews function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import Carousel from './components/Carousel';
 import NewsCategories from './components/NewsCategories';
 import SubscriptionSection from './components/SubscriptionSection';
 import Footer from './components/Footer';
+import Home from './components/Home'; 
+import Login from './components/Login'; 
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <HeroSection />
       <Carousel />
       <NewsCategories />
       <SubscriptionSection />
       <Footer />
-    </>
+    </Router>
   );
 }
 
 export default App;
+
--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,31 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home'; // Your homepage component
-import Login from './components/Login'; // Your login component
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import Carousel from './components/Carousel';
+import NewsCategories from './components/NewsCategories';
+import SubscriptionSection from './components/SubscriptionSection';
+import Footer from './components/Footer';
+import Home from './components/Home'; 
+import Login from './components/Login'; 
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/login" component={Login} />
-      </Switch>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <HeroSection />
+      <Carousel />
+      <NewsCategories />
+      <SubscriptionSection />
+      <Footer />
     </Router>
   );
 }
+
+export default App;
 
 
