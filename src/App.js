c8d8bfed65a620971c31e532d39ed7e75b3a2667
Chetana Baniya <chetanabaniya5@gmail.com>
2025-02-27 21:03:23 +0530

    Updated fetchNews function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import Carousel from './components/Carousel';
 import NewsCategories from './components/NewsCategories';
 import SubscriptionSection from './components/SubscriptionSection';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+      </Switch>
       <HeroSection />
       <Carousel />
       <NewsCategories />
       <SubscriptionSection />
       <Footer />
-    </>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to AI News Aggregator</h1>
+      <p>Your one-stop source for the latest news.</p>
+    </div>
+  );
+}
+
+
+
