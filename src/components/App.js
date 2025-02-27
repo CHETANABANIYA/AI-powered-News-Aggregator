c883601138f67adb4dffe67aac36738749d91917
Chetana Baniya <chetanabaniya5@gmail.com>
2025-02-27 20:44:52 +0530

    Updated fetchNews function

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,18 @@
+// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HeroSection from './components/HeroSection';
-import Carousel from './components/Carousel';
-import NewsCategories from './components/NewsCategories';
-import SubscriptionSection from './components/SubscriptionSection';
-import Footer from './components/Footer';
-import Home from './components/Home'; 
-import Login from './components/Login'; 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home'; // Your homepage component
+import Login from './components/Login'; // Your login component
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <HeroSection />
-      <Carousel />
-      <NewsCategories />
-      <SubscriptionSection />
-      <Footer />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+      </Switch>
     </Router>
   );
 }
-
-export default App;
 
 
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to AI News Aggregator</h1>
+      <p>Your one-stop source for the latest news.</p>
+    </div>
+  );
+}
+
+export default Home;
