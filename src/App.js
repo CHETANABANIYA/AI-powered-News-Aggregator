ee3059373409b259b8fb88c3c390cd9e3ca61d8b
Chetana Baniya <chetanabaniya5@gmail.com>
2025-03-03 01:17:08 +0530

    Improved index.js error handling and bootstrap imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
-import Carousel from './components/Carousel';
+import NewsCarousel from './components/NewsCarousel'; // ✅ Renamed to avoid conflict
 import NewsCategories from './components/NewsCategories';
 import SubscriptionSection from './components/SubscriptionSection';
 import Footer from './components/Footer';
@@ -12,24 +12,30 @@ import Login from './components/Login';
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={
-          <>
-            <HeroSection />
-            <Carousel />
-            <NewsCategories />
-            <SubscriptionSection />
-            <Footer />
-          </>
-        } />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <div> {/* ✅ Wrap everything in a div */}
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <HeroSection />
+                <NewsCarousel />
+                <NewsCategories />
+                <SubscriptionSection />
+                <Footer />
+              </div>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
 
-export default App; // ✅ Don't forget this!
+export default App;
+
 
 
 
